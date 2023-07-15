4b53365373ad20cda9e115f82e0150bac5f85428
Ehsan <eh3unm0radi@gmail.com>
2023-07-15 17:43:32 +0330

    1st commit

---
+++ admin/includes/side-nav.php
@@ -0,0 +1,31 @@
+    <div id="layoutSidenav_nav">
+        <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+            <div class="sb-sidenav-menu">
+                <div class="nav">
+
+                    <a class="nav-link" href="index.php">
+                        <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                        Dashboard
+                    </a>
+
+                    <a class="nav-link" href="users.php">
+                        <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
+                        Users
+                    </a>
+                    <a class="nav-link" href="upload.php">
+                        <div class="sb-nav-link-icon"><i class="fas fa-book-open"></i></div>
+                        Upload
+                    </a>
+
+                    <a class="nav-link" href="comments.php">
+                        <div class="sb-nav-link-icon"><i class="fas fa-chart-area"></i></div>
+                        Comments
+                    </a>
+                </div>
+            </div>
+            <div class="sb-sidenav-footer">
+                <div class="small">Logged in as:</div>
+                Start Bootstrap
+            </div>
+        </nav>
+    </div>
